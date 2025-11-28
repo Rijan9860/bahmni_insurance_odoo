00f25e050115559ca6dcf4d2d5dd32d8c74bed0f
Rijan9860 <rijanmaharjan12345@gmail.com>
2025-11-28 09:06:27 +0545

    Dynamic Payment Type

--- models/account_invoice.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
-
-    def action_generate_attachment(self):
-        pass
--- models/insurance_claim.py
+++ models/insurance_claim.py
@@ -42,75 +42,87 @@ class InsuranceClaim(models.Model):
             if not sale_order.nhis_number:
                 raise ValidationError("Claim can't be created. NHIS Number is not present.")
         
-        nmc_value = ""
-        if sale_order.provider_name:
-            nmc = sale_order.provider_name.split("_")
-            if len(nmc) > 1:
-                nmc_value = nmc[1]
-                _logger.info("NMC Value:%s", nmc_value)
+            nmc_value = ""
+            if sale_order.provider_name:
+                nmc = sale_order.provider_name.split("_")
+                if len(nmc) > 1:
+                    nmc_value = nmc[1]
+                    _logger.info("NMC Value:%s", nmc_value)
+                else:
+                    _logger.info("Invalid NMC Number")
             else:
-                _logger.info("Invalid NMC Number")
-        else:
-            _logger.info("Provider Number is Empty")
+                _logger.info("Provider Number is Empty")
 
-        visit_uuid = sale_order.external_visit_uuid
-        _logger.info("Visit UUID:%s", visit_uuid)
+            visit_uuid = sale_order.external_visit_uuid
+            _logger.info("Visit UUID:%s", visit_uuid)
 
-        if not visit_uuid:
-            _logger.info("Visit UUID is Empty")
+            if not visit_uuid:
+                _logger.info("Visit UUID is Empty")
 
-        insurance_number = sale_order.nhis_number
-        claim_code = sale_order.claim_id
+            insurance_number = sale_order.nhis_number
+            claim_code = sale_order.claim_id
 
-        claim = {
-            'nhis_number': insurance_number,
-            'claim_code': claim_code,
-            'claim_manager_id': sale_order.user_id.id,
-            'claimed_date': sale_order.create_date,
-            'partner_id': sale_order.partner_id.id,
-            'state': 'draft',
-            'partner_uuid': sale_order.partner_uuid,
-            'currency_id': sale_order.currency_id.id,
-            'sale_orders': sale_order,
-            'external_visit_uuid': visit_uuid,
-            'care_setting': sale_order.care_setting,
-            'nmc_number': nmc_value
-        }
-        _logger.info("Claim:%s", claim)
-        claim_in_db = self.env['insurance.claim'].search([('external_visit_uuid', '=', visit_uuid), ('care_setting', '=', 'ipd'), ('state', '=', 'draft')])
-        _logger.info("Existing IPD Claim Id:%s", claim_in_db)
-        # Create a insurance claim
-        if len(claim_in_db) == 0:
-            _logger.info("*****Creating New Claim*****")
-            claim_in_db = self.env['insurance.claim'].create(claim)
-            _logger.info("Claim in db:%s", claim_in_db)
+            claim = {
+                'nhis_number': insurance_number,
+                'claim_code': claim_code,
+                'claim_manager_id': sale_order.user_id.id,
+                'claimed_date': sale_order.create_date,
+                'partner_id': sale_order.partner_id.id,
+                'state': 'draft',
+                'partner_uuid': sale_order.partner_uuid,
+                'currency_id': sale_order.currency_id.id,
+                'sale_orders': sale_order,
+                'external_visit_uuid': visit_uuid,
+                'care_setting': sale_order.care_setting,
+                'nmc_number': nmc_value
+            }
+            _logger.info("Claim:%s", claim)
+            claim_in_db = self.env['insurance.claim'].search([('external_visit_uuid', '=', visit_uuid), ('care_setting', '=', 'ipd'), ('state', '=', 'draft')])
+            _logger.info("Existing IPD Claim Id:%s", claim_in_db)
+            # Create a insurance claim
+            if len(claim_in_db) == 0:
+                _logger.info("*****Creating New Claim*****")
+                claim_in_db = self.env['insurance.claim'].create(claim)
+                _logger.info("Claim in db:%s", claim_in_db)
 
-        # If care setting is "ipd" then adding new sales order
-        _logger.info("New Sale Order Id:%s", sale_order)
-        _logger.info("Old Sale Order Id:%s", claim_in_db.sale_orders)
+            # If care setting is "ipd" then adding new sales order
+            _logger.info("New Sale Order Id:%s", sale_order)
+            _logger.info("Old Sale Order Id:%s", claim_in_db.sale_orders)
 
-        claim_in_db.update({
-            'sale_orders': claim_in_db.sale_orders + sale_order
-        })
-        _logger.info("New Claim with added sale order:%s", claim_in_db)
-
-        # Create a insurance claim line
-        self._create_claim_line(claim_in_db, sale_order)
-
-        insurance_claim_lines = self.env['insurance.claim.line'].search([
-            ('claim_id', '=', claim_in_db.id)
-        ])
-
-        _logger.info("Insurance Claim Line:%s", insurance_claim_lines)
-        
-        # Update 'insurance claim line' id in the insurance claim model
-        if insurance_claim_lines:
             claim_in_db.update({
-                'insurance_claim_line': insurance_claim_lines
+                'sale_orders': claim_in_db.sale_orders + sale_order
             })
-        else:
-            _logger.info("No Claim Line Item Present")
-            raise ValidationError("No Claim Line Item Present")
+            _logger.info("New Claim with added sale order:%s", claim_in_db)
+
+            # Create a insurance claim line
+            self._create_claim_line(claim_in_db, sale_order)
+
+            insurance_claim_lines = self.env['insurance.claim.line'].search([
+                ('claim_id', '=', claim_in_db.id)
+            ])
+
+            _logger.info("Insurance Claim Line:%s", insurance_claim_lines)
+            
+            # Update 'insurance claim line' id in the insurance claim model
+            if insurance_claim_lines:
+                claim_in_db.update({
+                    'insurance_claim_line': insurance_claim_lines
+                })
+            else:
+                _logger.info("No Claim Line Item Present")
+                raise ValidationError("No Claim Line Item Present")
+            
+        #     # To generate pdf report in the ir.attachment model
+        #     account_move_id = self.env['account.move'].search([
+        #         ('invoice_origin', '=', sale_order.name)
+        #     ])
+        #     _logger.info("Sale Order Name:%s", sale_order.name)
+        #     _logger.info("Account Move Id:%s", account_move_id)
+        #     claim_id = claim_in_db
+        #     _logger.info("Claim Id:%s", claim_id)
+        #     self.env['account.move'].action_generate_attachment(account_move_id, claim_id)
+        # else:
+            _logger.info("Payment Type:%s", sale_order.payment_type)
        
     def _create_claim_line(self, claim, sale_order):
         _logger.info("Inside _create_claim_line")
